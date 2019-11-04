a85475806d7e956dd17b561d5c27cddbc810f209
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-11-04 14:55:39 +0100

    mvp done

--- accounts/accounts.js
+++ accounts/accounts.js
@@ -27,9 +27,41 @@ router.get("/:id", validateAccountId, (req, res) => {
     });
 });
 
-router.post("/", validateAccount, (req,res)=>{
-    
-})
+router.post("/", validateAccount, (req, res) => {
+  db.insert({ name: req.body.name, budget: req.body.budget })
+    .then(account => {
+      res.status(201).json(account);
+    })
+    .catch(error => {
+      res.status(500).json({ errorMessage: "Server error" + error });
+    });
+});
+
+router.put("/:id", validateAccountId, validateAccount, (req, res) => {
+  const id = req.params.id;
+  db.update(id, { name: req.body.name, budget: req.body.budget })
+    .then(account => {
+      res.status(201).json(account);
+    })
+    .catch(error => {
+      res.status(500).json({
+        errorMessage: "Server Error: " + error
+      });
+    });
+});
+
+router.delete("/:id", validateAccountId, (req, res) => {
+  const id = req.params.id;
+  db.remove(id)
+    .then(account => {
+      res.status(200).json(account);
+    })
+    .catch(error => {
+      res.status(500).json({
+        errorMessage: "Server Error: " + error
+      });
+    });
+});
 
 module.exports = router;
 
@@ -53,11 +85,11 @@ function validateAccountId(req, res, next) {
 }
 
 function validateAccount(req, res, next) {
-    if (Object.keys(req.body) == 0) {
-      res.status(404).json({ message: "Empty Account data." });
-    } else if (!req.body.name || !req.body.budget) {
-      res.status(404).json({ message: "missing required text field" });
-    } else {
-      next();
-    }
+  if (Object.keys(req.body) == 0) {
+    res.status(404).json({ message: "Empty Account data." });
+  } else if (!req.body.name || !req.body.budget) {
+    res.status(404).json({ message: "missing required text field" });
+  } else {
+    next();
   }
+}
